cb15f83ed56f05564e989e27a4e1f61cc40df805
Jonas <Jonas.m.Stahl@tum.com>
2022-04-03 23:58:30 +0200

    added viewcompetition

--- frontend/app/src/components/views/viewCompetitions.vue
+++ frontend/app/src/components/views/viewCompetitions.vue
@@ -1,11 +1,30 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import item from '@/components/views/viewItem.vue';
 
+const tournaments = ref([
+  {title: "Herren", description: "ganz viel Text"},
+  {title: "Damen", description: "noch mehr Text"},
+  {title: "U18", description: "richtig, noch mehr text"}
+])
 </script>
 
 <template>
-  <p>test2</p>
+  <div id="tournaments">
+    <item v-for="tournament in tournaments" :key="tournament.title" :title="tournament.title" :description="tournament.description" />
+  </div>
 </template>
 
-<style>
+<style scoped>
+#tournaments {
+  margin: 10px;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: center;
+}
 
+#tournaments > * {
+  margin: 0 10px 10px 10px;
+}
 </style>
