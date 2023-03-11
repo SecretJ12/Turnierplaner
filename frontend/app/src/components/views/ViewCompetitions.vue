19e810d497a1257f3417fbd33b55e2948d7e9e72
Jonas Stahl <Jonas.M.Stahl@gmail.com>
2023-03-11 23:07:07 +0100

    Add settings button to ViewCompetitions

--- frontend/app/src/components/views/ViewCompetitions.vue
+++ frontend/app/src/components/views/ViewCompetitions.vue
@@ -17,7 +17,8 @@ axios.get(`/competition/list?tourName=${route.params.tourId}`)
     .then((response) => {
       competitions.value = response.data
     })
-    .catch((error) => {
+    .catch((_) => {
+      // TODO show error
     })
 
 const isLoggedIn = inject('loggedIn', ref(false))
@@ -35,7 +36,8 @@ function checkCanEdit() {
           .then((response) => {
             canEdit.value = response.status === 200
           })
-          .catch((error) => {
+          .catch((_) => {
+            canEdit.value = false
           })
     }
   });
@@ -43,11 +45,21 @@ function checkCanEdit() {
 function addCompetition() {
   router.push({path: '/createCompetition'})
 }
+
+function settings() {
+  router.push({path: '/tournament/' + route.params.tourId + '/edit'})
+}
 </script>
 
 <template>
   <div id="container">
-    <h2>{{route.params.tourId}}</h2>
+    <h2>
+      <font-awesome-icon
+          @click="settings"
+          id="settings" :icon="['fas', 'gear']" class="fa-1x" >
+      </font-awesome-icon>
+      {{route.params.tourId}}
+    </h2>
     <div id="competitions">
       <item v-for="competition in competitions"
             :name="competition.name"
@@ -60,23 +72,36 @@ function addCompetition() {
 </template>
 
 <style scoped>
-#competitions {
-  margin: 10px;
-  display: flex;
-  flex-wrap: wrap;
-  flex-direction: row;
-  justify-content: center;
-}
+  #settings {
+    color: #303030;
+    margin-right: 5px;
+  }
 
-#container {
-  width: 100%;
-}
+  #settings:hover {
+    filter: drop-shadow(0 0 6px #808080);
+  }
 
-#competitions > * {
-  margin: 0 10px 10px 10px;
-}
+  #settings:active {
+    color: #505050;
+  }
 
-h2 {
-  text-align: center;
-}
+  #competitions {
+    margin: 10px;
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    justify-content: center;
+  }
+
+  #container {
+    width: 100%;
+  }
+
+  #competitions > * {
+    margin: 0 10px 10px 10px;
+  }
+
+  h2 {
+    text-align: center;
+  }
 </style>
