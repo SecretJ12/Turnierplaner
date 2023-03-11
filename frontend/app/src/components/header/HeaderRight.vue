7a525a2dbf159ec9ed555744e6dad492d257c292
Jonas Stahl <Jonas.M.Stahl@gmail.com>
2023-03-11 22:41:53 +0100

    Change language picker to element plus element

--- frontend/app/src/components/header/HeaderRight.vue
+++ frontend/app/src/components/header/HeaderRight.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { auth } from '/src/security/AuthService'
+import { auth } from '@/security/AuthService'
 import { ref, inject, watch } from 'vue'
 
 const currentUser = ref('')
@@ -21,6 +21,7 @@ function logout() {
   auth.logout();
 }
 
+const test = ref("")
 
 </script>
 
@@ -33,14 +34,16 @@ export default {
 }
 </script>
 
-
 <template>
   <div id="headerRight">
-    <div class="locale-changer">
-      <select v-model="$i18n.locale">
-        <option v-for="locale in $i18n.availableLocales" :key="`locale-${locale}`" :value="locale">{{ locale }}</option>
-      </select>
-    </div>
+    <el-select id="language-select" v-model="$i18n.locale" placeholder="ab">
+      <el-option
+          v-for="locale in $i18n.availableLocales"
+          :key="locale"
+          :label="locale"
+          :value="locale"
+      />
+    </el-select>
     <font-awesome-icon @click="login" v-if="!isLoggedIn" :icon="['fas', 'right-to-bracket']" class="fa-2x clickable" />
     <p v-if="isLoggedIn">{{ currentUser }}</p>
     <font-awesome-icon @click="logout" v-if="isLoggedIn" :icon="['fas', 'right-from-bracket']" class="fa-2x clickable" />
@@ -48,6 +51,10 @@ export default {
 </template>
 
 <style>
+  #language-select {
+    width: 20px;
+  }
+
   #headerRight {
     padding-right: 10px;
     display: flex;
