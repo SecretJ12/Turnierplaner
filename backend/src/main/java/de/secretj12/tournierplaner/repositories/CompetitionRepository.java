f7611df34b1c8388e5e08bb231a8c8d520f26660
Jonas Stahl <Jonas.M.Stahl@gmail.com>
2023-03-12 19:47:09 +0100

    Add api to create and edit competitions

--- backend/src/main/java/de/secretj12/tournierplaner/repositories/CompetitionRepository.java
+++ backend/src/main/java/de/secretj12/tournierplaner/repositories/CompetitionRepository.java
@@ -1,20 +1,19 @@
 package de.secretj12.tournierplaner.repositories;
 
 import de.secretj12.tournierplaner.entities.Competition;
-import de.secretj12.tournierplaner.entities.Tournament;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
-import org.jboss.resteasy.annotations.Query;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
 import javax.ws.rs.QueryParam;
 import java.util.List;
 import java.util.UUID;
 
 @ApplicationScoped
-public class CompetitionRepository implements PanacheRepository<Competition>  {
+public class CompetitionRepository implements PanacheRepository<Competition> {
+
+    public Competition getById(UUID uuid) {
+        return find("id", uuid).firstResultOptional().orElse(null);
+    }
 
     public Competition getByName(String tourName, String compName) {
         return find("#compByName",
--- backend/src/main/java/de/secretj12/tournierplaner/resources/CompetitionResource.java
+++ backend/src/main/java/de/secretj12/tournierplaner/resources/CompetitionResource.java
@@ -1,10 +1,14 @@
 package de.secretj12.tournierplaner.resources;
 
 import de.secretj12.tournierplaner.entities.Competition;
+import de.secretj12.tournierplaner.entities.Tournament;
 import de.secretj12.tournierplaner.repositories.CompetitionRepository;
+import de.secretj12.tournierplaner.repositories.TournamentRepository;
 import io.quarkus.security.identity.SecurityIdentity;
 
+import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -16,6 +20,8 @@ public class CompetitionResource {
     @Inject
     CompetitionRepository competitions;
     @Inject
+    TournamentRepository tournaments;
+    @Inject
     SecurityIdentity securityIdentity;
 
     @GET
@@ -23,7 +29,19 @@ public class CompetitionResource {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public List<Competition> getAllCompetitions(@QueryParam("tourName") String tourName) {
-        return competitions.listByName(tourName);
+        if (canSee(tourName))
+            return competitions.listByName(tourName);
+        return null;
+    }
+
+    @GET
+    @Path("/details")
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Competition getCompetition(@QueryParam("tourName") String tourName, @QueryParam("compName") String compName) {
+        if (canSee(tourName))
+            return competitions.getByName(tourName, compName);
+        return null;
     }
 
     @GET
@@ -36,11 +54,49 @@ public class CompetitionResource {
             return Response.status(Response.Status.UNAUTHORIZED).entity("Unauthorized").build();
     }
 
-    @GET
-    @Path("/details")
-    @Consumes(MediaType.TEXT_PLAIN)
-    @Produces(MediaType.APPLICATION_JSON)
-    public Competition getTournament(@QueryParam("tourName") String tourName, @QueryParam("compName") String compName) {
-        return competitions.getByName(tourName, compName);
+    @POST
+    @Path("/add")
+    @RolesAllowed("director")
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response addCompetition(Competition competition) {
+        if (competition.getTournament() == null)
+            return Response.status(Response.Status.BAD_REQUEST.getStatusCode(),
+                    "No tournament specified").build();
+        if (competitions.getByName(competition.getTournament().getName(), competition.getName()) != null)
+            return Response.status(Response.Status.CONFLICT).build();
+        Tournament tournament = tournaments.getByName(competition.getTournament().getName());
+        if (tournament == null)
+            return Response.status(Response.Status.BAD_REQUEST.getStatusCode(),
+                    "Tournament doesn't exist").build();
+
+        competition.setTournament(tournament);
+        competitions.persist(competition);
+        return Response.ok("successfully added").build();
+    }
+
+    @POST
+    @Path("/update")
+    @RolesAllowed("director")
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response updateCompetition(Competition competition) {
+        Competition savedCompetition = competitions.getById(competition.getId());
+        if (savedCompetition == null)
+            return Response.status(Response.Status.BAD_REQUEST.getStatusCode(),
+                    "Competition doesn't exist").build();
+
+        competitions.persist(savedCompetition);
+        savedCompetition.setName(competition.getName());
+        savedCompetition.setDescription(competition.getDescription());
+        savedCompetition.setType(competition.getType());
+
+        return Response.ok("successfully changed").build();
+    }
+
+    private boolean canSee(String tourName) {
+        return securityIdentity.hasRole("director") || tournaments.getByName(tourName).isVisible();
     }
 }
