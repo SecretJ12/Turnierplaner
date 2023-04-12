787d0fa143482c38bf93eece5b89009779be31d2
oliver <ge96xey@mytum.de>
2023-04-12 02:28:37 +0200

    changed ViewGroupSystem.vue from grid to table

--- frontend/app/src/components/views/competition/ViewGroupSystem.vue
+++ frontend/app/src/components/views/competition/ViewGroupSystem.vue
@@ -1,19 +1,34 @@
 <template>
-  <div class="container text-center" v-for="group in groupDetails.groups" >
-    <div>Group</div>
-    <div class="row" >
-      <div class="col" id="grid"></div>
-      <div class="col" v-for="team in group.teams" id="grid">
-        <!-- TODO second player       -->
-        <div>{{ team.playerA.firstName }} {{ team.playerA.lastName }}</div>
-      </div>
-    </div>
-    <div class="row" v-for="team in group.teams" >
-      <!--      TODO second player-->
-      <div class="col" id="grid">{{ team.playerA.firstName }} {{ team.playerA.lastName }}</div>
-      <div class="col" v-for="team in group.teams" id="grid">
-      </div>
-    </div>
+  <div class="container text-center" v-for="(group,index_group) in groupDetails.groups">
+    <table class="table">
+      <thead class="table-light">
+      <th scope="col">
+        {{ $t("ViewGroupSystem.group") }} {{ index_group +1}}
+      </th>
+      <th scope="col" v-for="(team) in group.teams" id="table-header"> {{ team.playerA.firstName }}
+        {{ team.playerA.lastName }}
+      </th>
+      </thead>
+      <tbody class="table-group-divider">
+      <tr v-for="(team,index_t1) in group.teams">
+        <th>{{ team.playerA.firstName }} {{ team.playerA.lastName }}</th>
+        <td v-for="(team,index_t2) in group.teams">
+          <div v-if="index_t1===index_t2">
+            /
+          </div>
+          <div v-else>
+            tba
+          </div>
+        </td>
+      </tr>
+      </tbody>
+      <caption v-if="progress[index_group]">
+        {{ $t("ViewGroupSystem.progress") }}
+      </caption>
+      <caption v-else>
+        {{ $t("ViewGroupSystem.finished") }}
+      </caption>
+    </table>
   </div>
 </template>
 
@@ -31,12 +46,14 @@ const groupDetails = reactive({
 await axios.get(`tournament/${route.params.tourId}/competition/${route.params.compId}/groupMatches`)
     .then((response) => {
       groupDetails.groups = response.data.groups
+      console.log(groupDetails.groups)
     })
     .catch((_) => {
     })
 
 console.log(groupDetails.groups)
 
+const progress = [false, true]
 
 </script>
 
@@ -45,6 +62,11 @@ console.log(groupDetails.groups)
 #grid {
   border: 1px solid #0a0a0a;
   border-radius: 2px;
+  background: #fbffff;
+}
+
+#table-header {
+  font-weight: normal;
 }
 
 </style>
