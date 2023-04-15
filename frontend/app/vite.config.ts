26bfbdbff72bdb78469dcf58498bc0f44209b365
Jonas Stahl <Jonas.M.Stahl@gmail.com>
2023-04-15 22:51:23 +0200

    Convert vite config to ts

--- frontend/app/vite.config.ts
+++ frontend/app/vite.config.ts
@@ -1,5 +1,5 @@
 import { fileURLToPath, URL } from 'url'
-
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -12,7 +12,7 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': resolve(__dirname, 'src')
     }
   },
   define: {
