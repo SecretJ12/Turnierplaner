4de66e8708f7c51898b437d65ea87a24dc215809
Jonas Stahl <Jonas.M.Stahl@gmail.com>
2023-08-13 20:40:36 +0200

    🔥 Remove old old Testdata generator

--- backend/src/main/java/de/secretj12/turnierplaner/startup/Startup.java
+++ backend/src/main/java/de/secretj12/turnierplaner/startup/Startup.java
@@ -1,17 +1,13 @@
 package de.secretj12.turnierplaner.startup;
 
 import io.quarkus.runtime.StartupEvent;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.event.Observes;
 import jakarta.inject.Inject;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @ApplicationScoped
 public class Startup {
-    @Inject
-    Testdata testdata;
-
     @Inject
     TestdataGenerator testdataGenerator;
 
@@ -21,7 +17,6 @@ public class Startup {
 
     void onStart(@Observes StartupEvent ev) {
         if (addTestdata) {
-//            testdata.createData();
             testdataGenerator.generateData();
         }
     }
