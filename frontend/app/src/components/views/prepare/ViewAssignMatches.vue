d9369255c97253587b4ecba5731a669e066d9fa5
Jonas Stahl <Jonas.M.Stahl@gmail.com>
2023-09-11 00:01:48 +0200

    ✨ Add outline for assigning matches

--- frontend/app/src/components/views/prepare/ViewAssignMatches.vue
+++ frontend/app/src/components/views/prepare/ViewAssignMatches.vue
@@ -1,8 +1,55 @@
 <template>
-	Assign matches: {{ competition?.name }}
+	<div class="grid">
+		<div class="col-4">
+			<Button class="mb-2"> Shuffle</Button>
+			<DataTable
+				:value="[{ name: 1 }, { name: 2 }, { name: 3 }, { name: 4 }]"
+				show-gridlines
+				striped-rows
+				removable-sort
+			>
+				<Column header="Player 1" field="name" />
+				<Column header="Player 2" field="name" />
+			</DataTable>
+		</div>
+
+		<!-- TODO some more drag'n'drop stuff -->
+		<template v-if="competition">
+			<template v-if="competition.tourType === TourType.GROUPS">
+				<DataTable
+					class="col-4"
+					v-for="i in [1,2]"
+					:key="i"
+					:value="[{ name: 1 }, { name: 2 }, { name: 3 }, { name: 4 }]"
+					show-gridlines
+					striped-rows
+					removable-sort
+				>
+					<Column :header="'Group ' + (i)" field="name">
+						<template #body>
+							<div
+								class="h-2rem border-dashed"
+								style="margin: -0.5rem; width: calc(100% + 1rem)"
+							></div>
+						</template>
+					</Column>
+				</DataTable>
+			</template>
+			<template v-else>
+				<div class="col-8 flex justify-content-center">
+					<ViewKnockoutTree
+						:match="knockoutSystem.finale"
+						:third-place="knockoutSystem.thirdPlace"
+						:mode="competition.mode"
+					/>
+				</div>
+			</template>
+		</template>
+	</div>
 
 	<div class="grid grid-nogutter justify-content-between mt-4">
 		<Button label="Back" icon="pi pi-angle-left" @click="prevPage" />
+		<Button :label="t('general.save')"> </Button>
 		<Button
 			v-if="route.params.step !== 'scheduleMatches'"
 			label="Next"
@@ -18,6 +65,9 @@ import { useRoute, useRouter } from "vue-router"
 import { useI18n } from "vue-i18n"
 import { getCompetitionDetails } from "@/backend/competition"
 import { useToast } from "primevue/usetoast"
+import ViewKnockoutTree from "@/components/views/competition/knockoutSystem/ViewKnockoutTree.vue"
+import { TourType } from "@/interfaces/competition"
+import { KnockoutMatch } from "@/interfaces/knockoutSystem"
 
 const route = useRoute()
 const router = useRouter()
@@ -29,6 +79,33 @@ const competition = getCompetitionDetails(route, t, toast, {
 		if (competition.value === null) return
 	},
 })
+
+function generateTree(size: number): KnockoutMatch {
+	return {
+		court: "a",
+		begin: new Date(),
+		end: new Date(),
+		finished: false,
+		winner: null,
+		teamA: null,
+		teamB: null,
+		sets: null,
+		curGame: null,
+		prevMatch:
+			size > 0
+				? {
+						winner: true,
+						a: generateTree(size - 1),
+						b: generateTree(size - 1),
+				  }
+				: undefined,
+	}
+}
+
+const knockoutSystem = {
+	finale: generateTree(1),
+	thirdPlace: generateTree(0),
+}
 
 function prevPage() {
 	router.replace({
