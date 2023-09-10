1ca20174391b9b28a1be40cd607d12e893d15727
Jonas Stahl <Jonas.M.Stahl@gmail.com>
2023-09-10 23:41:38 +0200

    🐛 Add outline for assigning teams

--- frontend/app/src/components/views/prepare/ViewAssignTeams.vue
+++ frontend/app/src/components/views/prepare/ViewAssignTeams.vue
@@ -1,11 +1,50 @@
 <template>
-	Assign teams: {{ competition?.name }}
+	<template
+		v-if="
+			competition?.mode === Mode.SINGLE ||
+			competition?.signUp === SignUp.TOGETHER
+		"
+	>
+		<!-- TODO internalization -->
+		There's nothing to do for this configuration
+	</template>
+	<template v-else>
+		<!-- TODO drag n drop stuff -->
+		<div class="grid">
+			<DataTable
+				class="col-4"
+				:value="[{ name: 1 }, { name: 2 }, { name: 3 }, { name: 4 }]"
+				show-gridlines
+				striped-rows
+				removable-sort
+			>
+				<Column header="Name" field="name" sortable />
+			</DataTable>
+			<DataTable
+				class="col-8"
+				:value="[{ name: 1 }, { name: 2 }, { name: 3 }, { name: 4 }]"
+				show-gridlines
+				striped-rows
+				removable-sort
+			>
+				<Column header="Player 1" field="name" sortabl>
+					<template #body>
+						<div class="h-2rem border-dashed" style="margin: -0.5rem; width: calc(100% + 1rem)"></div>
+					</template>
+				</Column>
+				<Column header="Player 2" field="name" sortabl>
+					<template #body>
+						<div class="h-2rem border-dashed" style="margin: -0.5rem; width: calc(100% + 1rem)"></div>
+					</template>
+				</Column>
+			</DataTable>
+		</div>
+	</template>
 
 	<div class="grid grid-nogutter justify-content-between mt-4">
 		<Button label="Back" icon="pi pi-angle-left" @click="prevPage" />
 		<!-- TODO add @click -->
 		<Button
-			v-if="route.params.step === 'editPlayers'"
 			:label="t('general.save')"
 		>
 		</Button>
@@ -24,6 +63,7 @@ import { useRoute, useRouter } from "vue-router"
 import { useI18n } from "vue-i18n"
 import { getCompetitionDetails } from "@/backend/competition"
 import { useToast } from "primevue/usetoast"
+import { Mode, SignUp } from "@/interfaces/competition"
 
 const route = useRoute()
 const router = useRouter()
@@ -51,4 +91,5 @@ function nextPage() {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+</style>
