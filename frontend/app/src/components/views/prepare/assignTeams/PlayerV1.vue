fbb4c3a85749cd3e5503cb42d948bf9a6fe43b41
Jonas Stahl <Jonas.M.Stahl@gmail.com>
2023-09-15 01:55:42 +0200

    🔥 Remove old assign team version

--- frontend/app/src/components/views/prepare/assignTeams/PlayerV1.vue
+++
@@ -1,9 +0,0 @@
-<template>
-	<div id="player" class="pointer-events-none border-solid border-1 flex align-items-center justify-content-center">{{props.name}}</div>
-</template>
-
-<script setup lang="ts">
-const props = defineProps<{ name: string }>()
-</script>
-
-<style scoped></style>
