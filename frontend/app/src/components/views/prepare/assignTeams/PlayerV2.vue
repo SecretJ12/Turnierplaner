2aa096cc0e136aeb8ba9307eb82160d60e88fa4f
Jonas Stahl <Jonas.M.Stahl@gmail.com>
2023-09-15 00:21:25 +0200

    🚧 Add another AssignTeam implementation

---
+++ frontend/app/src/components/views/prepare/assignTeams/PlayerV2.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="border-round select-none cursor-pointer inline p-3 text-50"
+		:class="{'bg-yellow-500': secondary, 'bg-primary-400': !secondary}"
+	>{{ props.player.name }}</div>
+</template>
+
+<script setup lang="ts">
+const props = withDefaults(defineProps<{ player: { name: string}, secondary: boolean }>(),
+	{
+		secondary: false
+	})
+</script>
+
+<style scoped>
+
+</style>
