701e7a6f39c09adccc7e9782504094ddc5f893e1
Jonas Stahl <Jonas.M.Stahl@gmail.com>
2023-09-18 23:10:15 +0200

    ✨ Implement tracking of list element of draggable

--- frontend/app/src/draggable/Draggable.vue
+++ frontend/app/src/draggable/Draggable.vue
@@ -1,56 +1,87 @@
 <template>
-	<component ref="container" :is="props.tag">
-		<slot v-for="(item, index) in list" :key="item['itemKey']" :item="item" :index="index" />
+	<component ref="container" :is="props.tag" :key="index">
+		<slot
+			v-for="(item, index) in list"
+			:key="item['itemKey']"
+			:item="item"
+			:index="index"
+		/>
 	</component>
 </template>
 
 <script setup lang="ts">
 // @ts-ignore
-import Sortable, { SortableEvent } from "sortablejs";
-import { ref, ssrContextKey, watch } from "vue";
+import Sortable, { SortableEvent } from "sortablejs"
+import { ref, watch } from "vue"
 
-const props = withDefaults(defineProps<{
-	list: any[], // TODO replace by generic component
-	itemKey: string,
-	tag: string,
-	group: string,
-	pull?: boolean | 'clone',
-	put?: boolean | string[],
-	animation?: number,
-	disabled?: boolean,
-	sort?: boolean,
-	ghost?: string
-}>(), {
-	animation: 150,
-	disabled: false,
-	sort: true,
-	pull: true,
-	put: true,
-	ghost: 'ghost'
-})
+const props = withDefaults(
+	defineProps<{
+		list: any[]
+		itemKey: string
+		tag: string
+		group: string
+		pull?: boolean | "clone"
+		put?: boolean | string[]
+		animation?: number
+		disabled?: boolean
+		sort?: boolean
+		ghost?: string
+	}>(),
+	{
+		animation: 150,
+		disabled: false,
+		sort: true,
+		pull: true,
+		put: true,
+		ghost: "ghost",
+	},
+)
 
-const container = ref<HTMLElement | null>(null);
+const container = ref<HTMLElement | null>(null)
 const sortable = ref<Sortable | null>()
 
-watch(container, (el) => {
-	if (!container)
-		return
+watch(container, () => {
+	if (!container.value) return
 
-	sortable.value = Sortable.create(el, {
+	sortable.value = Sortable.create(container.value, {
 		group: { name: props.group, pull: props.pull, put: props.put },
 		animation: props.animation,
 		disabled: props.disabled,
-		sort: props.sort,
+		sort: props.sort, // TODO enable tracking for move
 		ghostClass: props.ghost,
-		onEnd: (event: Sortable.SortableEvent) => {
-			console.log(event.oldIndex)
-			console.log(event.newIndex)
-			console.log(event)
+		onChoose: (event: Sortable.SortableEvent) => {
+			if (event.oldIndex === undefined) return
+			selectedElement = props.list[event.oldIndex]
+		},
+		onRemove: (event: Sortable.SortableEvent) => {
+			if (event.oldIndex === undefined) return
+			props.list.splice(event.oldIndex, 1)
+			reload()
+		},
+		onAdd: (event: Sortable.SortableEvent) => {
+			if (event.newIndex === undefined) return
+			// @ts-ignore
+			props.list.splice(event.newIndex, 0, selectedElement)
+			reload()
+		},
+		onEnd: ({ from, to, oldIndex, newIndex }) => {
+			if (from !== to) return
+			if (oldIndex === undefined || newIndex === undefined) return
+			props.list.splice(newIndex, 0, props.list.splice(oldIndex, 1)[0])
 		},
 	})
 })
+
+// reload list to ensure a consistent state
+const index = ref(0)
+
+function reload() {
+	index.value++
+}
+</script>
+<script lang="ts">
+// @ts-ignore
+let selectedElement = null
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
