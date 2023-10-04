3beb5b24ff7313d58f511ac77b292fdac7c080c5
Jonas Stahl <Jonas.M.Stahl@gmail.com>
2023-10-04 21:10:16 +0200

    ♻️ Add timeout for list for DeleteBox

--- frontend/app/src/components/views/prepare/editTeams/DeleteBox.vue
+++ frontend/app/src/components/views/prepare/editTeams/DeleteBox.vue
@@ -2,7 +2,8 @@
 	<div
 		class="h-3rem flex align-items-center pl-3 select-none w-9rem z-0"
 		style="position: absolute; margin-top: 3px"
-		@mouseover="showOp"
+		@mouseover="enter"
+		@mouseleave="leave"
 	>
 		<span class="text-red-500">Drop to delete</span>
 	</div>
@@ -15,37 +16,37 @@
 		:group="props.group"
 		class="flex flex-row flex-row flex-wrap gap-2 border-3 h-3rem border-round border-dashed w-10rem z-1"
 		style="border-color: var(--red-500); box-sizing: content-box"
-		@mouseover="showOp"
+		@mouseover="enter"
+		@mouseleave="leave"
 	>
 		<template #default="{ item }"></template>
 	</DraggablePanel>
 	<OverlayPanel ref="op">
-		<span v-if="trash.length === 0">No player deleted</span>
-		<DraggablePanel
-			id="trash"
-			:list="trash"
-			:put="[props.group]"
-			item-key="id"
-			tag="div"
-			:group="props.group"
-			class="flex flex-wrap gap-2 max-w-30rem"
-			style="border-color: var(--red-500)"
-			dismissable
-		>
-			<template #default="{ item }">
-				<PlayerCard
-					:id="item.name"
-					:player="item"
-				/>
-			</template>
-		</DraggablePanel>
+		<div @mouseover="enter" @mouseleave="leave">
+			<span v-if="trash.length === 0">No player deleted</span>
+			<DraggablePanel
+				id="trash"
+				:list="trash"
+				:put="[props.group]"
+				item-key="id"
+				tag="div"
+				:group="props.group"
+				class="flex flex-wrap gap-2 max-w-30rem"
+				style="border-color: var(--red-500)"
+				dismissable
+			>
+				<template #default="{ item }">
+					<PlayerCard :id="item.name" :player="item" />
+				</template>
+			</DraggablePanel>
+		</div>
 	</OverlayPanel>
 </template>
 
 <script setup lang="ts">
 import DraggablePanel from "@/draggable/DraggablePanel.vue"
 import { ref } from "vue"
-import { signedUpPlayer } from "@/interfaces/player";
+import { signedUpPlayer } from "@/interfaces/player"
 import PlayerCard from "@/components/views/prepare/editTeams/PlayerCard.vue"
 import OverlayPanel from "primevue/overlaypanel"
 
@@ -57,10 +58,18 @@ const trash = ref<signedUpPlayer[]>([])
 
 const op = ref<OverlayPanel>()
 
-function showOp(event: Event) {
+const timeout = 800
+let lastEnter = new Date()
+function enter(event: Event) {
+	lastEnter = new Date()
 	op.value?.show(event)
+}
+function leave() {
+	setTimeout(() => {
+		if (new Date().getTime() - lastEnter.getTime() > timeout)
+			op.value?.hide()
+	}, timeout)
 }
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
