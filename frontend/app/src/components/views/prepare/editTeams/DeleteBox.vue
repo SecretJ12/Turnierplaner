af964b6f5ee923f46f2b91f7426d5f0a11e01c2d
Jonas Stahl <Jonas.M.Stahl@gmail.com>
2023-10-04 22:00:27 +0200

    💄 Add div around DeleteBox

--- frontend/app/src/components/views/prepare/editTeams/DeleteBox.vue
+++ frontend/app/src/components/views/prepare/editTeams/DeleteBox.vue
@@ -1,46 +1,48 @@
 <template>
-	<div
-		class="h-3rem flex align-items-center pl-3 select-none w-9rem z-0"
-		style="position: absolute; margin-top: 3px"
-		@mouseover="enter"
-		@mouseleave="leave"
-	>
-		<span class="text-red-500">Drop to delete</span>
-	</div>
-	<DraggablePanel
-		id="trash"
-		:list="trash"
-		:put="[props.group]"
-		item-key="id"
-		tag="div"
-		:group="props.group"
-		class="flex flex-row flex-row flex-wrap gap-2 border-3 h-3rem border-round border-dashed w-10rem z-1"
-		style="border-color: var(--red-500); box-sizing: content-box"
-		@mouseover="enter"
-		@mouseleave="leave"
-	>
-		<template #default="{ item }"></template>
-	</DraggablePanel>
-	<OverlayPanel ref="op">
-		<div @mouseover="enter" @mouseleave="leave">
-			<span v-if="trash.length === 0">No player deleted</span>
-			<DraggablePanel
-				id="trash"
-				:list="trash"
-				:put="[props.group]"
-				item-key="id"
-				tag="div"
-				:group="props.group"
-				class="flex flex-wrap gap-2 max-w-30rem"
-				style="border-color: var(--red-500)"
-				dismissable
-			>
-				<template #default="{ item }">
-					<PlayerCard :id="item.name" :player="item" />
-				</template>
-			</DraggablePanel>
+	<div>
+		<div
+			class="h-3rem flex align-items-center pl-3 select-none w-9rem z-0"
+			style="position: absolute; margin-top: 3px"
+			@mouseover="enter"
+			@mouseleave="leave"
+		>
+			<span class="text-red-500">Drop to delete</span>
 		</div>
-	</OverlayPanel>
+		<DraggablePanel
+			id="trash"
+			:list="trash"
+			:put="[props.group]"
+			item-key="id"
+			tag="div"
+			:group="props.group"
+			class="flex flex-row flex-row flex-wrap gap-2 border-3 h-3rem border-round border-dashed w-10rem z-1"
+			style="border-color: var(--red-500); box-sizing: content-box"
+			@mouseover="enter"
+			@mouseleave="leave"
+		>
+			<template #default="{ item }"></template>
+		</DraggablePanel>
+		<OverlayPanel ref="op">
+			<div @mouseover="enter" @mouseleave="leave">
+				<span v-if="trash.length === 0">No player deleted</span>
+				<DraggablePanel
+					id="trash"
+					:list="trash"
+					:put="[props.group]"
+					item-key="id"
+					tag="div"
+					:group="props.group"
+					class="flex flex-wrap gap-2 max-w-30rem"
+					style="border-color: var(--red-500)"
+					dismissable
+				>
+					<template #default="{ item }">
+						<PlayerCard :id="item.name" :player="item" />
+					</template>
+				</DraggablePanel>
+			</div>
+		</OverlayPanel>
+	</div>
 </template>
 
 <script setup lang="ts">
@@ -58,7 +60,7 @@ const trash = ref<signedUpPlayer[]>([])
 
 const op = ref<OverlayPanel>()
 
-const timeout = 800
+const timeout = 300
 let lastEnter = new Date()
 function enter(event: Event) {
 	lastEnter = new Date()
