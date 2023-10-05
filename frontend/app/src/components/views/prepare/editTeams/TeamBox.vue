fccb1937ae79ba685b84482497aff1b4d7ef27d0
Jonas Stahl <Jonas.M.Stahl@gmail.com>
2023-10-06 01:15:20 +0200

    ✨  Add TeamBox

---
+++ frontend/app/src/components/views/prepare/editTeams/TeamBox.vue
@@ -0,0 +1,45 @@
+<template>
+	<div class="w-10rem">
+		<div
+			id="top"
+			class="border-3 border-dashed h-3rem border-round-top content-box"
+		></div>
+		<div
+			:class="{
+				bottomSame: !props.different,
+				bottomDifferent: props.different,
+			}"
+			class="border-3 border-top-none border-dashed h-3rem border-round-bottom content-box"
+		></div>
+	</div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+	different: boolean
+}>()
+</script>
+
+<style scoped>
+.content-box {
+	box-sizing: content-box;
+}
+
+#top {
+	border-left-color: var(--primary-400);
+	border-top-color: var(--primary-400);
+	border-right-color: var(--primary-400);
+}
+
+.bottomSame {
+	border-left-color: var(--primary-400);
+	border-bottom-color: var(--primary-400);
+	border-right-color: var(--primary-400);
+}
+
+.bottomDifferent {
+	border-left-color: var(--yellow-500);
+	border-bottom-color: var(--yellow-500);
+	border-right-color: var(--yellow-500);
+}
+</style>
--- frontend/app/src/components/views/prepare/editTeams/TeamList.vue
+++ frontend/app/src/components/views/prepare/editTeams/TeamList.vue
@@ -1,7 +1,10 @@
 <template>
 	<p>TeamList TODO</p>
+	<TeamBox different />
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import TeamBox from "@/components/views/prepare/editTeams/TeamBox.vue"
+</script>
 
 <style scoped></style>
