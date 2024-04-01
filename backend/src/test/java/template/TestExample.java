aa683aaeba44798a6a977e135749fc76f900936c
Jonas Stahl <Jonas.M.Stahl@gmail.com>
2024-04-01 23:41:58 +0200

    🎨 Apply formatter

--- backend/src/test/java/template/TestExample.java
+++ backend/src/test/java/template/TestExample.java
@@ -6,101 +6,67 @@ import jakarta.ws.rs.core.Response;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.is;
 
 @QuarkusTest
 public class TestExample {
 
     @Test
     public void unauthorized() {
-        given()
-            .get("/template/unauthorized")
-            .then().assertThat()
-            .statusCode(Response.Status.OK.getStatusCode());
+        given().get("/template/unauthorized").then().assertThat().statusCode(Response.Status.OK.getStatusCode());
     }
 
     @Test
     public void authorizedAsUnauthorized() {
-        given()
-            .get("/template/authorized")
-            .then().assertThat()
-            .statusCode(Response.Status.UNAUTHORIZED.getStatusCode());
+        given().get("/template/authorized").then().assertThat().statusCode(Response.Status.UNAUTHORIZED.getStatusCode());
     }
 
     @Test
     @TestSecurity(user = "testUser")
     public void authorizedAsAuthorized() {
-        given()
-            .get("/template/authorized")
-            .then().assertThat()
-            .statusCode(Response.Status.OK.getStatusCode());
+        given().get("/template/authorized").then().assertThat().statusCode(Response.Status.OK.getStatusCode());
     }
 
     @Test
     public void userAsUnauthorized() {
-        given()
-            .get("/template/user")
-            .then().assertThat()
-            .statusCode(Response.Status.UNAUTHORIZED.getStatusCode());
+        given().get("/template/user").then().assertThat().statusCode(Response.Status.UNAUTHORIZED.getStatusCode());
     }
 
     @Test
     @TestSecurity(user = "testUser", roles = {"user"})
     public void userAsUser() {
-        given()
-            .get("/template/user")
-            .then().assertThat()
-            .statusCode(Response.Status.OK.getStatusCode());
+        given().get("/template/user").then().assertThat().statusCode(Response.Status.OK.getStatusCode());
     }
 
     @Test
     public void reporterAsUnauthorized() {
-        given()
-            .get("/template/reporter")
-            .then().assertThat()
-            .statusCode(Response.Status.UNAUTHORIZED.getStatusCode());
+        given().get("/template/reporter").then().assertThat().statusCode(Response.Status.UNAUTHORIZED.getStatusCode());
     }
 
     @Test
     @TestSecurity(user = "testUser", roles = {"reporter"})
     public void reporterAsReporter() {
-        given()
-            .get("/template/reporter")
-            .then().assertThat()
-            .statusCode(Response.Status.OK.getStatusCode());
+        given().get("/template/reporter").then().assertThat().statusCode(Response.Status.OK.getStatusCode());
     }
 
     @Test
     public void directorAsUnauthorized() {
-        given()
-            .get("/template/director")
-            .then().assertThat()
-            .statusCode(Response.Status.UNAUTHORIZED.getStatusCode());
+        given().get("/template/director").then().assertThat().statusCode(Response.Status.UNAUTHORIZED.getStatusCode());
     }
 
     @Test
     @TestSecurity(user = "testUser", roles = {"director"})
     public void directorAsUser() {
-        given()
-            .get("/template/director")
-            .then().assertThat()
-            .statusCode(Response.Status.OK.getStatusCode());
+        given().get("/template/director").then().assertThat().statusCode(Response.Status.OK.getStatusCode());
     }
 
     @Test
     public void adminAsUnauthorized() {
-        given()
-            .get("/template/admin")
-            .then().assertThat()
-            .statusCode(Response.Status.UNAUTHORIZED.getStatusCode());
+        given().get("/template/admin").then().assertThat().statusCode(Response.Status.UNAUTHORIZED.getStatusCode());
     }
 
     @Test
     @TestSecurity(user = "testUser", roles = {"admin"})
     public void adminAsAdmin() {
-        given()
-            .get("/template/admin")
-            .then().assertThat()
-            .statusCode(Response.Status.OK.getStatusCode());
+        given().get("/template/admin").then().assertThat().statusCode(Response.Status.OK.getStatusCode());
     }
 }
