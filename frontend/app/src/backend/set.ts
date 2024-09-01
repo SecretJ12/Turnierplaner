8012df2afd750f60aedb4480fa1df15ea1bc99b1
oliver <ge96xey@mytum.de>
2024-09-01 21:26:56 +0200

    🚧 add scoring update for knockout matches

---
+++ frontend/app/src/backend/set.ts
@@ -0,0 +1,36 @@
+import { useMutation } from "@tanstack/vue-query"
+import { Set } from "@/interfaces/match"
+import axios from "axios"
+import { RouteLocationNormalizedLoaded } from "vue-router"
+import { ToastServiceMethods } from "primevue/toastservice"
+
+export function useUpdateSet(
+	route: RouteLocationNormalizedLoaded,
+	t: (s: string) => string,
+	toast: ToastServiceMethods,
+) {
+	return useMutation({
+		mutationFn: (data: { sets: Set[]; matchId: string }) =>
+			axios.post(
+				`/tournament/${<string>route.params.tourId}/competition/${data.matchId}/set`,
+				data.sets,
+			),
+		onSuccess() {
+			toast.add({
+				severity: "success",
+				summary: t("general.success"),
+				detail: t("general.saved"),
+				life: 3000,
+			})
+		},
+		onError(error) {
+			toast.add({
+				severity: "error",
+				summary: t("general.failure"),
+				detail: t("general.error_saving"),
+				life: 3000,
+			})
+			console.log(error)
+		},
+	})
+}
