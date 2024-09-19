d2dac4504888064b220dcfbe61d0dd01ce91e784
oliver <ge96xey@mytum.de>
2024-09-19 22:17:41 +0200

    ✨ use criteria api for filtering matches instead of stringbuilding

--- backend/src/main/java/de/secretj12/turnierplaner/db/repositories/MatchRepository.java
+++ backend/src/main/java/de/secretj12/turnierplaner/db/repositories/MatchRepository.java
@@ -4,12 +4,20 @@ import de.secretj12.turnierplaner.db.entities.Match;
 import de.secretj12.turnierplaner.db.entities.Player;
 import de.secretj12.turnierplaner.db.entities.Tournament;
 import de.secretj12.turnierplaner.db.entities.competition.Competition;
+import de.secretj12.turnierplaner.resources.jsonEntities.user.jUserMatch;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import io.quarkus.panache.common.Parameters;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import jakarta.transaction.Transactional;
 
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -21,6 +29,10 @@ public class MatchRepository implements PanacheRepository<Match> {
     FinalOfGroupRepository finalOfGroups;
     @Inject
     MatchOfGroupRepository matchOfGroups;
+
+    @Inject
+    EntityManager em;
+
 
     public Match findById(UUID id) {
         return find("id", id).firstResult();
@@ -34,35 +46,52 @@ public class MatchRepository implements PanacheRepository<Match> {
         return find("#nonGroupMatches", Parameters.with("comp", competition)).list();
     }
 
-    public List<Match> filterMatches(Tournament tournament, Competition competition,
-                                     Player player,
-                                     Instant from, Instant to) {
-        StringBuilder query = new StringBuilder("FROM Match m WHERE TRUE");
-        Parameters pars = new Parameters();
-        if (tournament != null) {
-            query.append(" AND m.competition.tournament = :tour");
-            pars.and("tour", tournament);
-        }
+    @Transactional
+    public List<jUserMatch> filterMatches(Tournament tournament, Competition competition,
+                                          Player player,
+                                          Instant from, Instant to) {
+
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Match> q = cb.createQuery(Match.class);
+        Root<Match> root = q.from(Match.class);
+        q.select(root);
+        Predicate predicate = applyAllValidPredicates(cb, root, q, tournament, competition, player, from, to);
+        q.where(predicate);
+
+        List<Match> result = em.createQuery(q).getResultList();
+        return result.stream().map(jUserMatch::new).toList();
+
+    }
+
+    private Predicate applyAllValidPredicates(CriteriaBuilder cb, Root<Match> root, CriteriaQuery<Match> query, Tournament tournament, Competition competition, Player player, Instant from, Instant to) {
+        List<Predicate> predicates = new ArrayList<>();
         if (competition != null) {
-            query.append(" AND m.competition = :comp");
-            pars.and("comp", competition);
+            predicates.add(cb.equal(root.get("competition"), competition));
+
+        }
+
+        if (tournament != null) {
+            predicates.add(cb.equal(root.get("competition").get("tournament"), tournament));
         }
 
         if (player != null) {
-            query.append(" AND (m.teamA.playerA = :player OR m.teamA.playerB = :player");
-            query.append(" OR m.teamB.playerA = :player OR m.teamB.playerB = :player)");
-            pars.and("player", player);
+            predicates.add(cb.or(
+                    cb.equal(root.get("teamA").get("playerA"), player),
+                    cb.equal(root.get("teamA").get("playerB"), player),
+                    cb.equal(root.get("teamB").get("playerA"), player),
+                    cb.equal(root.get("teamB").get("playerB"), player)
+            ));
         }
 
         if (from != null) {
-            query.append(" AND :from <= m.begin");
-            pars.and("from", from);
-        }
-        if (to != null) {
-            query.append(" AND m.end <= :to");
-            pars.and("to", to);
+            predicates.add(cb.greaterThanOrEqualTo(root.get("begin"), from));
         }
 
-        return find(query.toString(), pars).stream().toList();
+        if (to != null) {
+            predicates.add(cb.lessThanOrEqualTo(root.get("end"), to));
+        }
+
+        Predicate finalPredicate = cb.and(predicates.toArray(new Predicate[0]));
+        return finalPredicate;
     }
 }
--- backend/src/main/java/de/secretj12/turnierplaner/resources/MatchResource.java
+++ backend/src/main/java/de/secretj12/turnierplaner/resources/MatchResource.java
@@ -5,10 +5,13 @@ import de.secretj12.turnierplaner.db.entities.Player;
 import de.secretj12.turnierplaner.db.entities.Tournament;
 import de.secretj12.turnierplaner.db.entities.competition.Competition;
 import de.secretj12.turnierplaner.db.repositories.*;
-import de.secretj12.turnierplaner.resources.jsonEntities.user.jUserMatchEvent;
+import de.secretj12.turnierplaner.resources.jsonEntities.user.jUserMatch;
 import io.quarkus.security.identity.SecurityIdentity;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.*;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 
 import java.time.Instant;
@@ -33,24 +36,29 @@ public class MatchResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<jUserMatchEvent> getMatches(
-                                            @QueryParam("tour") String tourName,
-                                            @QueryParam("comp") String compName,
-                                            @QueryParam("player") String playerId,
-                                            @QueryParam("from") String from,
-                                            @QueryParam("to") String to
+    public List<jUserMatch> getMatches(
+            @QueryParam("tour") String tourName,
+            @QueryParam("comp") String compName,
+            @QueryParam("player") String playerId,
+            @QueryParam("from") String from,
+            @QueryParam("to") String to
     ) {
         Tournament tournament = tournaments.getByName(tourName);
         Competition competition = competitions.getByName(tourName, compName);
-        Player player = playerId == null ? null : players.getById(UUID.fromString(playerId));
-        Instant fromD = from == null ? null : Instant.parse(from);
-        Instant toD = to == null ? null : Instant.parse(to);
+        Player player = playerId.isEmpty() ? null : players.getById(UUID.fromString(playerId));
+        Instant fromD = from.isEmpty() ? null : Instant.parse(from);
+        Instant toD = to.isEmpty() ? null : Instant.parse(to);
 
-        if (player == null && tournament == null)
-            throw new BadRequestException("Need to specify at least a tournament or a player");
 
-        return this.matches.filterMatches(tournament, competition, player, fromD, toD).stream()
-            .map(jUserMatchEvent::new)
-            .toList();
+        System.out.println(competition);
+//        if (player == null && tournament == null)
+//            throw new BadRequestException("Need to specify at least a tournament or a player");
+
+        List<jUserMatch> matchList = matches.filterMatches(tournament, competition, player, fromD, toD);
+        System.out.println(matchList.size());
+        return matchList;
+//        return this.matches.filterMatches(tournament, competition, player, fromD, toD).stream()
+//            .map(jUserMatchEvent::new)
+//            .toList();
     }
 }
