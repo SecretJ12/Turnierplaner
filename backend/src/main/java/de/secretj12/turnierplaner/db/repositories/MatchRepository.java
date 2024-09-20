73938301badc167bcaf13e2079764ad0197821db
oliver <ge96xey@mytum.de>
2024-09-20 22:43:55 +0200

    🎨 ️add type safety

--- backend/src/main/java/de/secretj12/turnierplaner/db/repositories/MatchRepository.java
+++ backend/src/main/java/de/secretj12/turnierplaner/db/repositories/MatchRepository.java
@@ -1,9 +1,12 @@
 package de.secretj12.turnierplaner.db.repositories;
 
 import de.secretj12.turnierplaner.db.entities.Match;
+import de.secretj12.turnierplaner.db.entities.Match_;
 import de.secretj12.turnierplaner.db.entities.Player;
 import de.secretj12.turnierplaner.db.entities.Tournament;
 import de.secretj12.turnierplaner.db.entities.competition.Competition;
+import de.secretj12.turnierplaner.db.entities.competition.Competition_;
+import de.secretj12.turnierplaner.db.entities.competition.Team_;
 import de.secretj12.turnierplaner.resources.jsonEntities.user.jUserMatch;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import io.quarkus.panache.common.Parameters;
@@ -50,7 +53,6 @@ public class MatchRepository implements PanacheRepository<Match> {
     public List<jUserMatch> filterMatches(Tournament tournament, Competition competition,
                                           Player player,
                                           Instant from, Instant to) {
-
         CriteriaBuilder cb = em.getCriteriaBuilder();
         CriteriaQuery<Match> q = cb.createQuery(Match.class);
         Root<Match> root = q.from(Match.class);
@@ -60,37 +62,30 @@ public class MatchRepository implements PanacheRepository<Match> {
 
         List<Match> result = em.createQuery(q).getResultList();
         return result.stream().map(jUserMatch::new).toList();
-
     }
 
     private Predicate applyAllValidPredicates(CriteriaBuilder cb, Root<Match> root, CriteriaQuery<Match> query, Tournament tournament, Competition competition, Player player, Instant from, Instant to) {
         List<Predicate> predicates = new ArrayList<>();
         if (competition != null) {
-            predicates.add(cb.equal(root.get("competition"), competition));
-
+            predicates.add(cb.equal(root.get(Match_.COMPETITION), competition));
         }
-
         if (tournament != null) {
-            predicates.add(cb.equal(root.get("competition").get("tournament"), tournament));
+            predicates.add(cb.equal(root.get(Match_.COMPETITION).get(Competition_.TOURNAMENT), tournament));
         }
-
         if (player != null) {
             predicates.add(cb.or(
-                    cb.equal(root.get("teamA").get("playerA"), player),
-                    cb.equal(root.get("teamA").get("playerB"), player),
-                    cb.equal(root.get("teamB").get("playerA"), player),
-                    cb.equal(root.get("teamB").get("playerB"), player)
+                    cb.equal(root.get(Match_.TEAM_A).get(Team_.PLAYER_A), player),
+                    cb.equal(root.get(Match_.TEAM_A).get(Team_.PLAYER_B), player),
+                    cb.equal(root.get(Match_.TEAM_B).get(Team_.PLAYER_A), player),
+                    cb.equal(root.get(Match_.TEAM_B).get(Team_.PLAYER_B), player)
             ));
         }
-
         if (from != null) {
-            predicates.add(cb.greaterThanOrEqualTo(root.get("begin"), from));
+            predicates.add(cb.greaterThanOrEqualTo(root.get(Match_.BEGIN), from));
         }
-
         if (to != null) {
-            predicates.add(cb.lessThanOrEqualTo(root.get("end"), to));
+            predicates.add(cb.lessThanOrEqualTo(root.get(Match_.END), to));
         }
-
         Predicate finalPredicate = cb.and(predicates.toArray(new Predicate[0]));
         return finalPredicate;
     }
