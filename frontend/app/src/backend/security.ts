abae55d2ebf727133cb2f93cf44db3a474a9718c
Jonas Stahl <Jonas.M.Stahl@gmail.com>
2024-10-01 18:06:02 +0200

    ♻️ Allow reporters to report new results

--- frontend/app/src/backend/security.ts
+++ frontend/app/src/backend/security.ts
@@ -3,71 +3,86 @@ import { auth } from "@/security/AuthService"
 import axios from "axios"
 import { useQuery } from "@tanstack/vue-query"
 
-export function getIsDirector(isLoggedIn: Ref<boolean>) {
-	return useQuery({
-		queryKey: ["can_create", isLoggedIn],
-		queryFn: fetchIsDirector,
-		placeholderData: false,
-		staleTime: 0,
-	})
-}
-
-async function fetchIsDirector() {
-	const user = await auth.getUser()
-	if (user === null) return false
-
-	return await axios
-		.get<boolean>("/config/isDirector")
-		.then((response) => {
-			return response.data
-		})
-		.catch(() => {
-			return false
-		})
-}
-
 export function getCanEdit(tourId: string, isLoggedIn: Ref<boolean>) {
 	return useQuery({
 		queryKey: ["can_edit", tourId, isLoggedIn],
-		queryFn: () => fetchCanEdit(tourId),
+		queryFn: async () => {
+			const user = await auth.getUser()
+			if (user === null) return false
+
+			return await axios
+				.get<boolean>(`/tournament/${tourId}/competition/canEdit`)
+				.then((response) => {
+					return response.data
+				})
+				.catch(() => {
+					return false
+				})
+		},
 		placeholderData: false,
 		staleTime: 0,
 	})
-}
-
-async function fetchCanEdit(tourId: string) {
-	const user = await auth.getUser()
-	if (user === null) return false
-
-	return await axios
-		.get<boolean>(`/tournament/${tourId}/competition/canEdit`)
-		.then((response) => {
-			return response.data
-		})
-		.catch(() => {
-			return false
-		})
 }
 
 export function getIsAdmin(isLoggedIn: Ref<boolean>) {
 	return useQuery({
 		queryKey: ["isAdmin", isLoggedIn],
-		queryFn: () => fetchIsAdmin(),
+		queryFn: async () => {
+			const user = await auth.getUser()
+			if (user === null) return false
+
+			return await axios
+				.get<boolean>(`/config/isAdmin`)
+				.then((response) => {
+					return response.data
+				})
+				.catch(() => {
+					return false
+				})
+		},
 		placeholderData: false,
 		staleTime: 0,
 	})
 }
 
-async function fetchIsAdmin() {
-	const user = await auth.getUser()
-	if (user === null) return false
+export function getIsDirector(isLoggedIn: Ref<boolean>) {
+	return useQuery({
+		queryKey: ["isDirector", isLoggedIn],
+		queryFn: async () => {
+			const user = await auth.getUser()
+			if (user === null) return false
 
-	return await axios
-		.get<boolean>(`/config/isAdmin`)
-		.then((response) => {
-			return response.data
-		})
-		.catch(() => {
-			return false
-		})
+			return await axios
+				.get<boolean>("/config/isDirector")
+				.then((response) => {
+					return response.data
+				})
+				.catch(() => {
+					return false
+				})
+		},
+		placeholderData: false,
+		staleTime: 0,
+	})
+}
+
+export function getIsReporter(isLoggedIn: Ref<boolean>) {
+	return useQuery({
+		queryKey: ["isReporter", isLoggedIn],
+		queryFn: async () => {
+			const user = await auth.getUser()
+			if (user === null) return false
+
+			return await axios
+				.get<boolean>(`/config/isReporter`)
+				.then((response) => {
+					return response.data
+				})
+				.catch(() => {
+					return false
+				})
+		},
+		placeholderData: false,
+		staleTime: 0,
+	})
 }
