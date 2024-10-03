ea9e0d89144be8824ac2cecce236cf691c9e4565
Jonas Stahl <Jonas.M.Stahl@gmail.com>
2024-10-03 22:33:52 +0200

    🐛 Fix login

--- src/main/java/de/secretj12/turnierplaner/resources/MailTemplates.java
+++ src/main/java/de/secretj12/turnierplaner/resources/MailTemplates.java
@@ -7,7 +7,7 @@ import java.io.File;
 
 public class MailTemplates {
     //    TODO internationalization
-    String url = "http://localhost:3000";
+    String url = "http://localhost:2000";
 
     public Mail verificationMail(String recipient, String verificationcode) {
         return Mail
